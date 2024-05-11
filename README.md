5dec89710d10e07784435af07a948d56b35f739f
Lothrik <maximius@gmail.com>
2024-05-10 17:12:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# diablo4-dev-transcripts
+ 
+Diablo 4 developer update and interview transcripts, generated with https://github.com/openai/whisper and https://github.com/yt-dlp/yt-dlp.
---
+++ generate-transcripts.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from pathlib import Path
+import whisper
+
+target_folder = Path(__file__).parent
+source_files = []
+
+model = whisper.load_model("base", device="cuda")
+print("Whisper model loaded.")
+
+def transcribe_audio(path):
+    transcribe = model.transcribe(audio=path, language="en")
+    segments = transcribe["segments"]
+
+    output = ""
+    for segment in segments:
+        startTime = "0" + str(timedelta(seconds=int(segment['start']))) + ",000"
+        endTime = "0" + str(timedelta(seconds=int(segment['end']))) + ",000"
+        text = segment["text"]
+        segmentId = segment["id"] + 1
+        segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] is ' ' else text}\n\n"
+        output = output + segment
+
+    return output
+
+for source_file in source_files:
+    source_path = (target_folder / source_file).as_posix()
+    target_path = (target_folder / source_file.replace(".webm", ".srt")).as_posix()
+    transcript = transcribe_audio(source_path)
+    with open(target_path, "w", encoding="utf-8") as f:
+        f.write(transcript)
+    print("Finished processing: " + source_file)
