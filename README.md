d50972dd1caadb7bd4df8f6a44453b6b77b1e7e7
Lothrik <maximius@gmail.com>
2024-11-22 21:39:48 -0800

    Improve generate-transcripts.py, add documentation
    Closes #1
    
    Co-Authored-By: pikess <16213626+pikess@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # diablo4-dev-transcripts
  
 Diablo 4 update and interview transcripts generated with https://github.com/openai/whisper and https://github.com/yt-dlp/yt-dlp.
+
+# how to use
+
+Step 1: install python (3.11 or later is recommended):  https://www.python.org/downloads/
+
+Step 2: install ffmpeg (or add an existing installation to your PATH):  https://www.ffmpeg.org/download.html
+
+Step 3: install CUDA:  https://developer.nvidia.com/cuda-downloads
+
+Step 4: uninstall PyTorch:  `pip3 uninstall torch torchvision torchaudio`
+
+Step 5: reinstall PyTorch:  https://pytorch.org/
+
+Step 6: install OpenAI-Whisper:  `pip3 install -U openai-whisper`
+
+Step 7: configure `model_size`, `audio_lang`, `source_ext`, and `target_folder` if required in `generate-transcripts.py`
+
+Step 8: run `generate-transcripts.py` via python3
--- generate-transcripts.py
+++ generate-transcripts.py
@@ -1,32 +1,83 @@
 from datetime import timedelta
 from pathlib import Path
+import glob
+import sys
+import subprocess
+import torch
 import whisper
 
+# turbo is the recommended model size, use medium or small if you encounter issues
+model_size = "turbo" # https://github.com/openai/whisper#available-models-and-languages
+audio_lang = "en"
+
+# ".mp4" ".mkv" ".mov" ".webm" ".mp3" ".wav" ".m4a" -- anything ffmpeg can decode will work
+source_ext = ".mp4"
+
+# this is where the script will look for `source_ext` files, and where it will save transcripts
 target_folder = Path(__file__).parent / "transcripts"
-source_files = []
 
-model = whisper.load_model("base", device="cuda")
-print("Whisper model loaded.")
+def ffmpeg_installed():
+    try:
+        # try to run "ffmpeg -version" to check if it's available
+        result = subprocess.run(["ffmpeg", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode == 0:
+            return True
+        else:
+            return False
+    except FileNotFoundError:
+            return False
 
-def transcribe_audio(path):
-    transcribe = model.transcribe(audio=path, language="en")
+def transcribe_audio(path, audio_lang):
+    transcribe = model.transcribe(audio=path, language=audio_lang)
     segments = transcribe["segments"]
 
     output = ""
     for segment in segments:
-        startTime = "0" + str(timedelta(seconds=int(segment["start"]))) + ",000"
-        endTime = "0" + str(timedelta(seconds=int(segment["end"]))) + ",000"
+        startTime = f'0{str(timedelta(seconds=int(segment["start"])))},000'
+        endTime = f'0{str(timedelta(seconds=int(segment["end"])))},000'
         text = segment["text"]
         segmentId = segment["id"] + 1
+        if len(text) == 0:
+            print(f"WARN: `transcribe_audio` returned zero-length text at segment {segmentId}.")
+            text = " "
         segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
         output = output + segment
 
     return output
 
-for source_file in source_files:
-    source_path = (target_folder / source_file).as_posix()
-    target_path = (target_folder / source_file.replace(".mp4", ".srt")).as_posix()
-    transcript = transcribe_audio(source_path)
-    with open(target_path, "w", encoding="utf-8") as f:
-        f.write(transcript)
-    print("Finished processing: " + source_file)
+if __name__ == "__main__":
+    print(f'PyTorch version: {torch.__version__}')
+    print(f'CUDA version: {torch.version.cuda}')
+    print(f'Device name: {torch.cuda.get_device_properties("cuda").name}')
+    print(f'FlashAttention available: {torch.backends.cuda.flash_sdp_enabled()}')
+
+    if not ffmpeg_installed():
+        print("ERROR: ffmpeg not installed or otherwise unavailable. Please verify installation and check PATH parameter.")
+        sys.exit(0)
+
+    torch_device = "cuda"
+    if not torch.cuda.is_available():
+        torch_device = "cpu"
+        print("WARN: CUDA not installed or otherwise unavailable. Generating transcripts will be much slower than normal.")
+
+    if not target_folder.is_dir():
+        print(f'ERROR: `{target_folder}` does not point to a valid directory.')
+        sys.exit(0)
+
+    source_files = glob.glob(f"*{source_ext}", root_dir=target_folder)
+
+    model = whisper.load_model(model_size, device=torch_device)
+    print("Whisper model loaded.")
+
+    print(f"Target folder: `{target_folder}` contains {len(source_files)} `{source_ext}` files, processing...")
+    for source_file in source_files:
+        source_path = (target_folder / source_file).as_posix()
+        target_path = (target_folder / source_file.replace(source_ext, ".srt")).as_posix()
+        if Path(target_path).is_file():
+            print(f"Skipped processing: {source_file}")
+        else:
+            print(f"Started processing: {source_file}")
+            transcript = transcribe_audio(source_path, audio_lang)
+            with open(target_path, "w", encoding="utf-8") as f:
+                f.write(transcript)
+            print(f"Finished processing: {source_file}")
